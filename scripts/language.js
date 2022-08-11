bdddd1f5535735498d5c0c206dbffedebb9d3d01
Artyomkin141z <gilimov2artyom@gmail.com>
2022-08-11 13:51:17 +0300

    change language

---
+++ scripts/language.js
@@ -0,0 +1,45 @@
+// import language from "./lngTranslate";
+
+const ruSwitch = document.querySelector(".switch__RU");
+const engSwitch = document.querySelector(".switch__ENG");
+const allLang = ['ru', 'eng'];
+
+ruSwitch.addEventListener('click', (event) => {
+    location.href = window.location.pathname + "#ru";
+    location.reload();
+});
+engSwitch.addEventListener("click", (event) => {
+    location.href = window.location.pathname + "#eng";
+    location.reload();
+});
+
+function changeLanguage(){
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    
+    if(!allLang.includes(hash)){
+        location.href = window.location.pathname + "#eng";
+        location.reload();
+    }
+
+    if(hash == "ru"){
+        ruSwitch.classList.add("switch__click");
+        engSwitch.classList.remove("switch__click");
+    }
+    else{
+        engSwitch.classList.add("switch__click");
+        ruSwitch.classList.remove("switch__click");
+    }
+
+    document.title = language['name'][hash];
+
+    for(let i in language){
+        let selectors = document.querySelectorAll('.lng-' + i);
+        for(let n = 0; n < selectors.length; n++){
+            selectors[n].innerHTML = language[i][hash];
+        }
+        console.log(selectors);
+    }
+}
+
+changeLanguage();
---
+++ scripts/lngTranslate.js
@@ -0,0 +1,40 @@
+const language = {
+    "home":{
+        "ru": "Домой",
+        "eng": "Home"
+    },
+    "aboutMe":{
+        "ru":"Обо мне",
+        "eng":"About me"
+    },
+    "scills":{
+        "ru":"Скилы",
+        "eng":"Scills"
+    },
+    "portfolio":{
+        "ru":"Проекты",
+        "eng":"Portfolio"
+    },
+    "contacts":{
+        "ru":"Контакты",
+        "eng":"Contacts"
+    },
+    "name":{
+        "ru":"Артем Гилимович",
+        "eng":"Artyom Gilimovich"
+    },
+    "job":{
+        "ru":"Front-end разработчик<br>19 лет, Минск",
+        "eng":"Front-end developer<br>19 years old, Minsk"
+    },
+    "aboutMeContent":{
+        "ru":"Привет, я Артем и тд",
+        "eng":"Hi, I'm Denis – UX/UI designer from Minsk.I'm interested in design and everything connected with it.<br><br>I'm studying at courses \"Web and mobile design interfaces\" in IT-Academy.<br><br>Ready to implement excellent projectswith wonderful people."
+    },
+    "contactsContent":{
+        "ru":"Хочешь знать большк или просто пообщаться? Не стесняйтесь!",
+        "eng":"Want to know more or just chat? You are welcome!"
+    }
+}
+
+// export default language;
